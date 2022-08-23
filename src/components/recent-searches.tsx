a3a8a99de8ad074a00b478dcf80ec325b033bd26
xunjoy <36482777+xunjoy@users.noreply.github.com>
2022-08-23 15:38:55 -0700

    ui pretty

--- src/components/recent-searches.tsx
+++ src/components/recent-searches.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import Result from '../types/results'
+import '../App.css';
 
 // const arrOfObj = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
@@ -16,12 +17,12 @@ export default function RecentSearches () {
     <div >
         <h2>Things we have searched for...</h2>
         <div className="App-recentSearches">
-            <div className="">12</div>
-            <div className="item">wow</div>
-            <div className="item">wowowowowowowow</div>
-            <div className="item">Recent search query 4</div>
-            <div className="item">Recent search query 5</div>
-            <div className="item">Recent search query 6</div>
+            <div className="item">heal</div>
+            <div className="item">indigenous</div>
+            <div className="item">wikipedia</div>
+            <div className="item">technology</div>
+            <div className="item">salad</div>
+            <div className="item">current events</div>
         </div>
       
     </div>
